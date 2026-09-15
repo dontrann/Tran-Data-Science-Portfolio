8cd28592396fce0e7ebf391ca9893506b383049b
dontrann <dtran2@nd.edu>
2026-09-15 10:14:04 -0400

    update app

--- week_3/my_app.py
+++ week_3/my_app.py
@@ -7,3 +7,21 @@ if st.button("Click me!"):
     st.write("🎉 You clicked the button! Nice work! 🚀")
 else:
     st.write("Click the button to see what happens...")
+
+import pandas as pd
+
+st.subheader ("Exploring Our Dataset")
+
+df = pd.read_csv("data/sample_data.csv")
+
+    # if in week_2
+    
+
+st.write ("Here's out data")
+st.dataframe(df)
+
+city = st.selectbox("Select a city", df["City"].unique())
+st.write(f"People in {city}")
+st.dataframe(df[df["City"] == city])
+
+st.bar_chart(df["Salary"])
